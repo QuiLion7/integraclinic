e0327b5ffeea3f12f9852abbbf8deaa542db6896
QuiLion7 <quilbrub@gmail.com>
2025-03-30 23:53:25 -0300

    feat(layout): implement base application layout with conditional header and footer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
+	import { page } from '$app/state';
+
+	let { children } = $props();
+</script>
+
+{#if page.url.pathname !== '/' && page.url.pathname !== '/login'}
+	<Header />
+{/if}
+
+<main class="min-h-screen bg-gradient-to-br from-sky-50 to-sky-100">
+	{#if children}
+		{@render children()}
+	{/if}
+</main>
+
+{#if page.url.pathname !== '/' && page.url.pathname !== '/login'}
+	<Footer />
+{/if}
